3f9d53636d680b18d7c72c04141c3807541016bc
Maxime Houis <maxime.houis@epitech.eu>
2022-02-06 15:20:36 +0100

    feat: first VertexBuffer draft

---
+++ lib/src/Vertex.cpp
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2021-2021 [fill name later]
+ *
+ * This software is provided "as-is", without any express or implied warranty. In no event
+ *     will the authors be held liable for any damages arising from the use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose, including commercial
+ *     applications, and to alter it and redistribute it freely, subject to the following restrictions:
+ *
+ * 1. The origin of this software must not be misrepresented; you must not claim that you
+ *     wrote the original software. If you use this software in a product, an acknowledgment
+ *     in the product documentation would be appreciated but is not required.
+ *
+ * 2. Altered source versions must be plainly marked as such, and must not be misrepresented
+ * as being the original software.
+ *
+ * 3. This notice may not be removed or altered from any source distribution.
+ */
+
+#include "Vulk/Vertex.hpp"
+
+vk::VertexInputBindingDescription Vertex::getBindingDescription() noexcept
+{
+    vk::VertexInputBindingDescription bindingDescription{};
+
+    bindingDescription.binding = 0;
+    bindingDescription.stride = sizeof(Vertex);
+    bindingDescription.inputRate = vk::VertexInputRate::eVertex;
+
+    return bindingDescription;
+}
+
+Vertex::AttributeDescriptions Vertex::getAttributeDescriptions() noexcept
+{
+    AttributeDescriptions attributeDescriptions{};
+
+    attributeDescriptions[0].binding = 0;
+    attributeDescriptions[0].location = 0;
+    attributeDescriptions[0].format = vk::Format::eR32G32Sfloat;
+    attributeDescriptions[0].offset = offsetof(Vertex, position);
+
+    return attributeDescriptions;
+}
