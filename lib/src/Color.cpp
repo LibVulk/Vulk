4816cbe78cf7e63dd4553f66daf857fdc5ff8c61
Maxime Houis <maxime.houis@epitech.eu>
2022-02-28 02:26:25 +0100

    feat: add color struct & tests

---
+++ lib/src/Color.cpp
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2021-2021 [fill name later]
+ *
+ * This software is provided "as-is", without any express or implied warranty. In no event
+ *     will the authors be held liable for any damages arising from the use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose, including commercial
+ *     applications, and to alter it and redistribute it freely, subject to the following restrictions:
+ *
+ * 1. The origin of this software must not be misrepresented; you must not claim that you
+ *     wrote the original software. If you use this software in a product, an acknowledgment
+ *     in the product documentation would be appreciated but is not required.
+ *
+ * 2. Altered source versions must be plainly marked as such, and must not be misrepresented
+ * as being the original software.
+ *
+ * 3. This notice may not be removed or altered from any source distribution.
+ */
+
+#include "Vulk/Color.hpp"
+
+vulk::Color vulk::Color::Red{255, 0, 0, 255};
+vulk::Color vulk::Color::Green{0, 255, 0, 255};
+vulk::Color vulk::Color::Blue{0, 0, 255, 255};
+vulk::Color vulk::Color::Magenta{255, 0, 255, 255};
+vulk::Color vulk::Color::Cyan{0, 255, 255, 255};
+vulk::Color vulk::Color::Yellow{255, 255, 0, 255};
+vulk::Color vulk::Color::Black{0, 0, 0, 255};
+vulk::Color vulk::Color::White{255, 255, 255, 255};
+vulk::Color vulk::Color::Grey{127, 127, 127, 255};
+vulk::Color vulk::Color::Transparent{0, 0, 0, 0};
+vulk::Color vulk::Color::CoolYellow{0xffca00ff};
